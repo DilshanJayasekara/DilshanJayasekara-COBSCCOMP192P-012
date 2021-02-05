b3c6900985fa73421c4f309085f4cebdbd43ce16
Dilshan <130susith@gmail.com>
2021-02-05 15:21:03 +0530

    Initial Commit

---
+++ DilshanJayasekara-COBSCCOMP192P-012/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DilshanJayasekara-COBSCCOMP192P-012
+//
+//  Created by Dilshan Jayasekara on 2/5/21.
+//  Copyright © 2021 Dilshan Jayasekara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
